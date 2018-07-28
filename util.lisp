ab88d4032476a1b471dcfce8c809f63ae2fe2bfa
cxxxr <g23tlm@gmail.com>
2018-07-28 19:42:12 +0900

    base-charのstreamに対して日本語の書込み時でエラーになっていたのを回避

--- util.lisp
+++ util.lisp
@@ -1,5 +1,13 @@
 (in-package :simple-http-server)
 
+(defun apply-with-error-handle (function &rest args)
+  (handler-bind ((error (lambda (condition)
+                          (format *log-stream* "~A~%" condition)
+                          (uiop:print-backtrace :stream *log-stream*
+                                                :condition condition)
+                          (return-from apply-with-error-handle))))
+    (apply function args)))
+
 (defun rfc7231-date ()
   (multiple-value-bind (second minute hour date month year day)
       (get-decoded-time)
@@ -8,10 +16,10 @@
 		 ~D ~2,'0D:~2,'0D:~2,'0D GMT"
             day date month year hour minute second)))
 
-(defun apply-with-error-handle (function &rest args)
-  (handler-bind ((error (lambda (condition)
-                          (format *log-stream* "~A~%" condition)
-                          (uiop:print-backtrace :stream *log-stream*
-                                                :condition condition)
-                          (return-from apply-with-error-handle))))
-    (apply function args)))
+(defun read-file-base-char-string (filename)
+  (with-open-file (stream  filename
+                           :direction :input
+                           :element-type '(unsigned-byte 8))
+    (let ((buffer (make-array (file-length stream))))
+      (read-sequence buffer stream)
+      (map 'string 'code-char buffer))))
