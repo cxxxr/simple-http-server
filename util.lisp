a0a3dbd8e7558c39da20badf6374e1f3e17b378e
cxxxr <g23tlm@gmail.com>
2018-08-08 19:47:18 +0900

    rfc7231ではなくrfc1123だったようなので修正

--- util.lisp
+++ util.lisp
@@ -8,9 +8,8 @@
                           (return-from apply-with-error-handle))))
     (apply function args)))
 
-(defun rfc7231-date ()
-  (multiple-value-bind (second minute hour date month year day)
-      (get-decoded-time)
+(defun rfc-1123-date (&optional (time (get-universal-time)))
+  (multiple-value-bind (second minute hour date month year day) (decode-universal-time time)
     (format nil "~[Mon~;Tue~;Wed~;Thu~;Fri~;Sat~;Sun~], ~2,'0D ~
 		 ~[Jan~;Feb~;Mar~;Apr~;May~;Jun~;Jul~;Aug~;Sep~;Oct~;Nov~;Dec~] ~
 		 ~D ~2,'0D:~2,'0D:~2,'0D GMT"
